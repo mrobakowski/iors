c2a775513570826e8616dce5e2d75bd00e78f344
mrobakowski <mikolaj.rob@gmail.com>
2020-09-28 23:22:23 +0200

    build the jvm part in the integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,23 +1,42 @@
 use jni::objects::JObject;
 use jni::{Executor, InitArgsBuilder, JavaVM};
 use once_cell::sync::Lazy;
-use std::{path::PathBuf, sync::Arc};
+use std::ops::Deref;
+use std::process::Command;
+use std::{env, path::PathBuf, sync::Arc};
 
 static IORS_PATH: Lazy<PathBuf> = Lazy::new(test_cdylib::build_current_project);
+static JAR_PATH: Lazy<PathBuf> = Lazy::new(|| {
+    let sbt_path = env::var("SBT_PATH").expect("SBT_PATH must be set and pointing to the sbt jar");
+    let java = env::var("JAVA_HOME").map_or("java".into(), |p| {
+        let mut p = PathBuf::from(p);
+        p.push("bin");
+        p.push("java.exe");
+        p.to_string_lossy().to_string()
+    });
+
+    Command::new(java)
+        .args(&["-jar", &sbt_path, "assembly"])
+        .current_dir("./iors-jvm")
+        .status()
+        .unwrap();
+
+    PathBuf::from("./iors-jvm/target/scala-2.13/iors-jvm-assembly-0.1.jar")
+});
 
 #[test]
 fn jni_works() {
     let lib_path = format!(
         "-Djava.library.path={}",
-        IORS_PATH.parent().unwrap().to_string_lossy(),
+        IORS_PATH.parent().unwrap().display(),
     );
-    let jar_path = "-Djava.class.path=./iors-jvm/target/scala-2.13/iors-jvm-assembly-0.1.jar";
+    let jar_path = format!("-Djava.class.path={}", JAR_PATH.deref().display());
 
     let jvm = Arc::new(
         JavaVM::new(
             InitArgsBuilder::new()
                 .option(&lib_path)
-                .option(jar_path)
+                .option(&jar_path)
                 .option("-Xcheck:jni")
                 .build()
                 .unwrap(),
